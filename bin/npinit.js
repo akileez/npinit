7ae0b29c4d82d55ba86ac9edd5a4fc695730d21f
akileez <git@keeda.net>
2015-03-26 22:23:00 -0400

    refactor
    major overhaul and additions

--- bin/npinit.js
+++ bin/npinit.js
@@ -1,13 +1,15 @@
 #!/usr/bin/env node
-const argv = require('argh').argv;
-const fs   = require('fs');
-const meta = require('./meta');
-const cc   = require('change-case');
-const clog = require('jsome');
-const init = require('../');
+const argv = require('argh').argv
+const fs   = require('fs')
+const meta = require('../lib/meta')
+const dasher = require('lodash/string/kebabCase')
+const clog = require('jsome')
+const init = require('../')
+const exec   = require('child_process').exec
+const assert = require('assert')
 
-process.stdout.on('error', process.exit);
-// console.log(argv.argv)
+// process.stdout.on('error', process.exit)
+// console.log(argv)
 
 // help
 const noCommands = process.argv.length <= 2 && process.stdin.isTTY;
@@ -23,10 +25,14 @@ if (argv.v || argv.version) {
   process.exit(0)
 }
 
+// package name
 if (argv.argv) {
-  var projName = cc.paramCase(argv.argv[0]).replace(/\//g, '-');
+  var projName = dasher(argv.argv[0]).replace(/\//g, '-')
+} else if (argv.n || argv.new || argv.d || argv.dry) {
+  var projName = 'testproj' + Math.floor(Math.random() * (1000 - 101) + 101)
 } else {
-  var projName = 'testproj' + Math.floor(Math.random() * (1000 - 101) + 101) ;
+  process.stdout.write(fs.readFileSync(__dirname + '/usage.txt', 'utf-8') + '\n')
+  process.exit(0)
 }
 
 // default options
@@ -45,33 +51,49 @@ var opts = {
   meta: {
     packageName: projName,
     type: 'private',
-    repo: 'none'
+    version: '0.1.0',
+    license: 'MIT',
+    repo: 'null'
   },
   inputs: {
-    license: '',
     author: '',
-    email: ''
+    email: '',
+    user: ''
   }
-};
-
-if (argv.desc || argv.description) {
-  opts.meta.description = argv.desc || argv.description;
-} else {
-  opts.meta.description = 'No description given'
 }
 
-if (argv.tags) {
-  opts.meta.tags = argv.tags.split()
-} else {
-  opts.meta.tags = "none"
+// user information
+author(opts, function (author) {
+  opts.meta.author = author
+})
+email(opts, function (email) {
+  opts.meta.email = email
+})
+
+function author (conf, cb) {
+  exec('npm config get init.author.name', function (err, author) {
+    assert.ifError(err)
+    author = author.replace(/(\n)/gm, '')
+    cb(author)
+  })
 }
 
+function email (conf, cb) {
+  exec('npm config get init.author.email', function (err, email) {
+    assert.ifError(err)
+    email = email.replace(/(\n)/gm, '')
+    cb(email)
+  })
+}
+
+// git repo initialization
 if (argv.g || argv.git) {
   opts.git = true
   opts.meta.repo = 'init'
   opts.files.gitignore = true
 }
 
+// public repo
 if (argv.p || argv.public ) {
   opts.meta.type = "public"
   opts.files.license = true
@@ -81,10 +103,29 @@ if (argv.p || argv.public ) {
   opts.meta.repo = 'init'
 }
 
-if (argv.license) opts.inputs.license = argv.license
+// description for package.json
+if (argv.desc || argv.description) {
+  opts.meta.description = argv.desc || argv.description;
+} else {
+  opts.meta.description = 'null'
+}
+
+// tags for package.json
+if (argv.tags) {
+  opts.meta.tags = argv.tags.split()
+} else {
+  opts.meta.tags = 'null'
+}
+
+// optional overrides
+if (argv.license) opts.meta.license = argv.license
+if (argv.pkgv)    opts.meta.version = argv.pkgv
 if (argv.author)  opts.inputs.author = argv.author
 if (argv.email)   opts.inputs.email = argv.email
+if (argv.user)    opts.inputs.user  = argv.user
 
+
+// getDate and npm user then create project
 meta(opts, function (nwOpts) {
   if (argv.d || argv.dry) {
     clog(nwOpts);
