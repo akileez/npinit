544ef1a08ba93619d0bbff16b3a5a3e8c1b6617e
akileez <git@keeda.net>
2015-03-30 17:00:01 -0400

    update usage and option checks

--- bin/npinit.js
+++ bin/npinit.js
@@ -35,18 +35,40 @@ function usage () {
     Options:
 
       -h, --help          output usage information
+
       -v, --version       output the version number
+
       -n, --new           new private module. for use without a packageName. the default
                           packageName `testproj###` will be assigned. random number `###`
                           generated to avoid potential conflicts.
+
+      -g, --github        new public module. a git repository will be
+                          automactically initialized and pushed to gihub. a packageName
+                          is required. [default mode is private module, no git repo]
+
       -d, --dry           dry run displaying metadata used for generation
-      -g, --github        create public module. a git repository will be
-                          automactically initialized and pushed to gihub.
-                          [default mode is private/local module, no git repo]
-      -r, --repo          initialize a git repository [default is none]
-      --noPush            do not push repository to github. use with flags -g or --github
-      --desc <string>     description for package.json. enclose the string in quotes
-                          i.e., "This is an awesome project"
+
+      -r, --repo          initialize a git repository when generating a private module
+                          [default is none]
+
+      --addRemote         process will use the generic git command:
+
+                              `git remote add origin git@github.com:username/repo.git`
+
+                          username and repo will be automactically added. leaving this option
+                          off will use `hub` (https://github.com/github/hub) to create the remote
+                          repository. [default command `hub create`]
+
+      --noRemote          do not create a remote repository on github. noRemote assumes noPush and will
+                          override addRemote if both are present. it will also override the default
+                          remote command `hub create` if addRemote is not present when creating a
+                          public module with flags -g or --github.
+
+      --noPush            do not push repository to github. use only with flags -g or --github [default is push]
+
+      --desc <string>     description for package.json and github repository if using `hub`. enclose
+                          the string in quotes, i.e., "This is an awesome project"
+
       --tags <string>     keywords for package.json. use a comma separate list of items
                           i.e., "apple, orange, pear"
 
@@ -97,7 +119,8 @@ var opts = {
     version: '0.1.0',
     license: 'MIT',
     repo: 'null',
-    push: false
+    noPush: false,
+    noRemote: false
   }
 }
 
@@ -123,7 +146,19 @@ if (!pubpriv && pub) {
   opts.files.gitignore = true
   opts.git = true
   opts.meta.repo = 'init'
-  opts.meta.push = argv.noPush ? false : true
+  chkRemote()
+}
+
+function chkRemote () {
+  if (argv.noRemote) {
+    opts.meta.noRemote = true
+    opts.meta.noPush = true
+    opts.meta.remoteCmd = null
+  } else {
+    opts.meta.noRemote = false
+    opts.meta.noPush = argv.noPush ? true : false
+    opts.meta.remoteCmd = argv.addRemote ? "addRemote" : "hubCreate"
+  }
 }
 
 // description for package.json
