0a06a58a7114046e8cbc91261301505917ba31bc
akileez <git@keeda.net>
2015-04-08 18:21:37 -0400

    refactor install process
    the beginnings of the install refactor for callbacks.

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const install = require('./lib/install')
 const write = require('./lib/write')
 const git = require('./lib/git')
+const tim = require('./lib/timeout')
 const mkdir = require('mkdirp')
 
 module.exports = init
@@ -8,13 +9,25 @@ module.exports = init
 // Init writing files
 // @param {Object} opts
 function init (opts) {
-  const files = opts.files
   const pn = opts.meta.packageName
-  const writer = write(opts.meta)
 
   mkdir.sync(pn)
   process.chdir(pn)
 
+  tmpls(opts, function (done) {
+    if (opts.install) install()
+    else if (opts.git) git(opts)
+    else console.log('We are done')
+  })
+}
+
+function tmpls (opts, cb) {
+  const writer = write(opts.meta)
+  const files = opts.files
+
+  console.log('')
+  console.log('Templates:')
+  console.log('')
   if (files.gitignore) writer('./.gitignore', '../templates/gitignore')
   if (files.index) writer('./index.js', '../templates/index.js')
   if (files.license) writer('./LICENSE', '../templates/LICENSE')
@@ -22,6 +35,7 @@ function init (opts) {
   if (files.readme) writer('./README.md', '../templates/README.md')
   if (files.test) writer('./test.js', '../templates/test.js')
   if (files.travis) writer('./.travis.yml', '../templates/travis.yml')
-  if (opts.install) install()
-  if (opts.git) git(opts)
+  tim(50)(function () {
+    cb(true)
+  })
 }
