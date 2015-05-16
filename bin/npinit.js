9740b0a18a219db4ecf15dc4cf85cd4db49ae74d
akileez <git@keeda.net>
2015-05-16 09:50:10 -0400

    reorder functions
    — moved functions to bottom of file
    — move usage to separate file
    — create usage function to require help text
    — create projName function instead of variable assignment

--- bin/npinit.js
+++ bin/npinit.js
@@ -20,101 +20,6 @@ if (chk4help || noCommands) usage()
 // version
 if (argv.v || argv.version) vers()
 
-// package name
-if (argv.argv) var projName = makePkgName(false)
-else if (chkstate) var projName = makePkgName(true)
-else usage()
-
-function usage () {
-  console.log(function () {
-  /*
-
-  Usage: npinit <packageName> [options]
-         npinit -n [options]
-         npinit -dry [options]
-
-  Options:
-
-    -h, --help          output usage information
-
-    -v, --version       output the version number
-
-    --dry               dry run displaying metadata used for generation
-
-    -n, --new           new private module. for use without a packageName. the default
-                        packageName `testproj###` will be assigned. random number `###`
-                        generated to avoid potential conflicts.
-
-    -g, --github        new public module. a git repository will be
-                        automactically initialized and pushed to gihub. a packageName
-                        is required. [default mode is private module, no git repo]
-                        running option -n or --new with -g or --github is equivalent to
-                        option `-nr` or `--new --repo` initializing a new private
-                        module with a git repository. packageName will not be required.
-                        options --addRemote, --noRemote and --noPush are inactive in
-                        this mode when `-ng` are run together.
-
-    -r, --repo          initialize a git repository when generating a private module
-                        [default is none]
-
-    --addRemote         process will use the generic git command:
-
-                          `git remote add origin https://github.com/username/repo.git`
-
-                        username and repo will be automactically added. Leaving this
-                        option off will use `hub` (https://github.com/github/hub)
-                        to createthe remote repository. [default command `hub create`].
-                        You will have to enter your github username and password
-                        if NOT using the --noPush option.
-
-    -R, --noRemote      do not create a remote repository on github. noRemote
-                        assumes noPush and will override addRemote if both are present.
-                        it will also override the default remote command `hub create`
-                        if addRemote is not present when creating a public module
-                        with flags -g or --github.
-
-    -P, --noPush        do not push repository to github. use only with
-                        flags -g or --github [default is push]
-
-    --D <string>        a list of node modules to install, i.e.,
-                        `--D "lodash moment"` or `--D "lodash, moment"`.
-                        [npm i --save packages]
-
-    --d <string>        a list of node dev modules to install, i.e.,
-                        `--d "tape istanbul"` or `--d "tape, istanbul"`.
-                        [npm i --save-dev devpackages]
-
-  Overrides:
-
-    --desc    <string>  description for package.json and github repository if using `hub`.
-    --author  <string>  author name for project. [default reads from .npmrc or 'Your Name']
-    --email   <string>  email for project. [default reads from .npmrc or 'your@email.com']
-    --user    <string>  github username [default reads from .npmrc or 'githubName']
-    --license <string>  license type for project. [default reads from .npmrc => ISC]
-    --pkgv    <string>  semantic version for project [default reads from .npmrc => 1.0.0]
-
-  */
-  }.toString().split(/\n/).slice(2, -2).join('\n'))
-  process.exit(0)
-}
-
-function vers () {
-  process.stdout.write(require('../package.json').version + '\n')
-  process.exit(0)
-}
-
-function makePkgName (choice) {
-  var testName = 'testproj' + Math.floor(Math.random() * (1000 - 101) + 101)
-
-  switch (choice) {
-    case false:
-      var tmp = argv.argv[0].toString()
-      if (tmp.length === 0) return testName
-      return slug(argv.argv[0])
-    case true: return testName
-  }
-}
-
 // default options
 // ///////////////////////////////////////////////////////////////////////////////
 var opts = {
@@ -134,7 +39,7 @@ var opts = {
   meta: {
     date: new Date().toLocaleString(),
     year: new Date().getFullYear().toString(),
-    packageName: projName,
+    packageName: projName(),
     type: 'private',
     repo: 'none',
     remoteCmd: 'none',
@@ -148,13 +53,6 @@ var opts = {
 
 // git repo initialization
 if (argv.r || argv.repo) repo()
-
-// configure private git repository
-function repo () {
-  opts.git = true
-  opts.meta.repo = 'init'
-  opts.files.gitignore = true
-}
 
 // check for private and public projects being created together
 const pubpriv = (argv.g || argv.github) && (argv.n || argv.new)
@@ -173,40 +71,11 @@ if (!pubpriv && pub) {
 // private repo if options -g or --github and -n together
 if (pubpriv && pub) repo()
 
-function chkRemote () {
-  if (argv.noRemote || argv.R) {
-    opts.meta.noRemote = true
-    opts.meta.noPush = true
-  } else {
-    opts.meta.noRemote = false
-    opts.meta.noPush = argv.noPush || argv.P ? true : false
-  }
-}
-
-function remotecommand () {
-  opts.meta.remoteCmd = 'hubCreate'
-  if (argv.addRemote) opts.meta.remoteCmd = 'addRemote'
-  if (argv.noRemote || argv.R) opts.meta.remoteCmd = 'no public repository'
-}
-
 // install dependencies configuration
 // ///////////////////////////////////////////////////////////////////////////////
 // check for user install dependencies
 if (argv.d && argv.d !== true) devpackages()
 if (argv.D && argv.D !== true) packages()
-
-// configure user installed devDependencies
-function devpackages () {
-  opts.devpackages = compact(words(argv.d, /(\w?-?)*/g))
-  opts.files.test = true
-  opts.install = true
-}
-
-// configure user installed dependencies
-function packages () {
-  opts.packages = compact(words(argv.D, /(\w?-?)*/g))
-  opts.install = true
-}
 
 // misc overrides
 // ///////////////////////////////////////////////////////////////////////////////
@@ -231,3 +100,68 @@ meta(opts, function (nwOpts) {
     init(nwOpts)
   }
 })
+
+// configuration functions
+// ///////////////////////////////////////////////////////////////////////////////
+function usage () {
+  return require('../lib/usage')()
+}
+
+function vers () {
+  process.stdout.write(require('../package.json').version + '\n')
+  process.exit(0)
+}
+
+function projName () {
+  if (argv.argv) return makePkgName(false)
+  else if (chkstate) return makePkgName(true)
+  else usage()
+}
+
+function makePkgName (choice) {
+  var testName = 'testproj' + Math.floor(Math.random() * (1000 - 101) + 101)
+
+  switch (choice) {
+    case false:
+      var tmp = argv.argv[0].toString()
+      if (tmp.length === 0) return testName
+      return slug(argv.argv[0])
+    case true: return testName
+  }
+}
+
+// configure private git repository
+function repo () {
+  opts.git = true
+  opts.meta.repo = 'init'
+  opts.files.gitignore = true
+}
+
+function chkRemote () {
+  if (argv.noRemote || argv.R) {
+    opts.meta.noRemote = true
+    opts.meta.noPush = true
+  } else {
+    opts.meta.noRemote = false
+    opts.meta.noPush = argv.noPush || argv.P ? true : false
+  }
+}
+
+function remotecommand () {
+  opts.meta.remoteCmd = 'hubCreate'
+  if (argv.addRemote) opts.meta.remoteCmd = 'addRemote'
+  if (argv.noRemote || argv.R) opts.meta.remoteCmd = 'no public repository'
+}
+
+// configure user installed devDependencies
+function devpackages () {
+  opts.devpackages = compact(words(argv.d, /(\w?-?)*/g))
+  opts.files.test = true
+  opts.install = true
+}
+
+// configure user installed dependencies
+function packages () {
+  opts.packages = compact(words(argv.D, /(\w?-?)*/g))
+  opts.install = true
+}
