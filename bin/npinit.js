bc76005277149b326cd482a4b30decda7be6f65c
akileez <git@keeda.net>
2015-06-08 17:10:39 -0400

    update
    — move usage back to command line function npinit for performance
    reasons.
    — removed lib/usage.js
    — replace some with json-colorz
    — prep tag release

--- bin/npinit.js
+++ bin/npinit.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 const argv = require('argh').argv
-const clog = require('jsome')
+const clog = require('json-colorz')
 const clrz = require('colorz')
 const init = require('../')
 const log = require('../lib/log')
@@ -82,7 +82,7 @@ if (description) opts.meta.description = description
 else opts.meta.description = 'An awesome module being created'
 
 // tags for package.json
-// if (argv.tags) opts.meta.tags = argv.tags.split()
+// if (argv.tags) opts.meta.tags = makeArray(argv.tags)
 // else opts.meta.tags = ''
 
 // get user information, display options & initialize project
@@ -102,7 +102,77 @@ meta(opts, function (nwOpts) {
 // configuration functions
 // ///////////////////////////////////////////////////////////////////////////////
 function usage () {
-  return require('../lib/usage')()
+  console.log(function () {
+  /*
+
+  Usage: npinit <packageName> [options]
+         npinit <packageName> --dry [options]
+         npinit -n [options]
+         npinit -dry [options]
+
+  Options:
+
+    -h, --help          output usage information
+
+    -v, --version       output the version number
+
+    --dry               dry run displaying metadata used for generation
+
+    -n, --new           new private module. for use without a packageName. the default
+                        packageName `testproj###` will be assigned. random number `###`
+                        generated to avoid potential conflicts.
+
+    -g, --github        new public module. a git repository will be
+                        automactically initialized and pushed to gihub. a packageName
+                        is required. [default mode is private module, no git repo]
+                        running option -n or --new with -g or --github is equivalent to
+                        option `-nr` or `--new --repo` initializing a new private
+                        module with a git repository. packageName will not be required.
+                        options --addRemote, --noRemote and --noPush are inactive in
+                        this mode when `-ng` are run together.
+
+    -r, --repo          initialize a git repository when generating a private module
+                        [default is none]
+
+    --addRemote         process will use the generic git command:
+
+                          `git remote add origin https://github.com/username/repo.git`
+
+                        username and repo will be automactically added. Leaving this
+                        option off will use `hub` (https://github.com/github/hub)
+                        to createthe remote repository. [default command `hub create`].
+                        You will have to enter your github username and password
+                        if NOT using the --noPush option.
+
+    -R, --noRemote      do not create a remote repository on github. noRemote
+                        assumes noPush and will override addRemote if both are present.
+                        it will also override the default remote command `hub create`
+                        if addRemote is not present when creating a public module
+                        with flags -g or --github.
+
+    -P, --noPush        do not push repository to github. use only with
+                        flags -g or --github [default is push]
+
+    --D <string>        a list of node modules to install, i.e.,
+                        `--D "lodash moment"` or `--D "lodash, moment"`.
+                        [npm i --save packages]
+
+    --d <string>        a list of node dev modules to install, i.e.,
+                        `--d "tape istanbul"` or `--d "tape, istanbul"`.
+                        [npm i --save-dev devpackages]
+
+  Overrides:
+
+    --desc    <string>  description for package.json and github repository if using `hub`.
+    --author  <string>  author name for project. [default reads from .npmrc or 'Your Name']
+    --email   <string>  email for project. [default reads from .npmrc or 'your@email.com']
+    --user    <string>  github username [default reads from .npmrc or 'githubName']
+    --license <string>  license type for project. [default reads from .npmrc => ISC]
+    --pkgv    <string>  semantic version for project [default reads from .npmrc => 1.0.0]
+
+  */
+  }.toString().split(/\n/).slice(2, -2).join('\n'))
+  process.exit(0)
 }
 
 function vers () {
