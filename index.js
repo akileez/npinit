5e35e6af414b24cecc066f6e89f892d29c0f7653
akileez <git@keeda.net>
2015-09-29 13:00:12 -0400

    refactor: update index.js (getUserInfo)
    start moving in processes to get general template working correctly.
    getUserInfo now displaying correctly as before. need to start
    refactoring internals for re-configuration of newly desired
    functionality.

--- index.js
+++ index.js
@@ -4,59 +4,75 @@
  * Copyright (c) 2015 Keith Williams.
  * Licensed under the ISC license.
  */
-var argv        = require('argh').argv
+
 var mkdirp      = require('mkdirp')
 var meta        = require('./app/meta')
-var install     = require('./app/install')
-var initgit     = require('./app/initgit')
-var installdeps = require('./app/installDependencies')
-var iterate     = require('./lib/iterate')
+var iterate     = require('toolz/src/async/iterate')
+var assert      = require('assert')
+var clog        = require('json-colorz')
+var clrz        = require('colorz')
+// var install     = require('./app/install')
+// var initgit     = require('./app/initgit')
+// var installdeps = require('./app/installDependencies')
+// var iterate     = require('./lib/iterate')
 var logger      = require('./lib/logger')
 
+function proc (opts, argv) {
 
-var operations = [
-  initConfig,
-  userInfo,
-  createDir,
-  installFiles,
-  installDependencies,
-  initRepo,
-]
+  var operations = [
+    getUserInfo
+    // createDir,
+    // installFiles,
+    // installDependencies,
+    // initRepo,
+  ]
 
-iterate.each(operations, function (err) {
-  assert.ifError(err)
-  console.log('all done')
-})
-
-function userInfo (cb) {
-  meta(opts, argv, function (res) {
-    opts = res
-    cb(null)
+  iterate.series(operations, function (err) {
+    assert.ifError(err)
+    console.log('all done')
   })
+
+  function getUserInfo (cb) {
+    meta(opts, argv, function (res) {
+      opts = res
+      logger(res)
+      if (argv.dry) {
+        process.stdout.write('\n')
+        process.stdout.write(clrz.cyan('Options'))
+        process.stdout.write('\n\n')
+        clog(res)
+        cb(null)
+      } else {
+        cb(null)
+      }
+    })
+  }
+
+  // function createDir (cb) {
+  //   mkdirp(opts.meta.packageName, function (err) {
+  //     process.chdir(opts.meta.packageName)
+  //     cb(null)
+  //   })
+  // }
+
+  // function installFiles (cb) {
+  //   install(opts, function (res) {
+  //     // something here maybe?
+  //     cb(null)
+  //   })
+  // }
+
+  // function installDependencies (cb) {
+  //   installdeps(opts, function (res) {
+  //     cb(null)
+  //   })
+  // }
+
+  // function initRepo (cb) {
+  //   initgit(opts, function (res) {
+  //     cb(null)
+  //   })
+  // }
 }
 
-function createDir (cb) {
-  mkdirp(opts.meta.packageName, function (err) {
-    process.chdir(opts.meta.packageName)
-    cb(null)
-  })
-}
-
-function installFiles (cb) {
-  install(opts, function (res) {
-    // something here maybe?
-    cb(null)
-  })
-}
-
-function installDependencies (cb) {
-  installdeps(opts, function (res) {
-    cb(null)
-  })
-}
-
-function initRepo (cb) {
-  initgit(opts, function (res) {
-    cb(null)
-  })
-}
+module.exports = proc
