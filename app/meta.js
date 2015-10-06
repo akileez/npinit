893e5cd0008ce246cdc30b4b9c11d39af23f1b0b
akileez <git@keeda.net>
2015-10-06 17:41:49 -0400

    refactor: re-write app/meta.js
    one function now…

--- app/meta.js
+++ app/meta.js
@@ -1,31 +1,19 @@
 const assert = require('assert')
-const nconf = require('npmconf')
+const nconf  = require('npmconf')
 
 // user information & optional overrides
-function makeConfig (opts, cb) {
-  return getMeta(opts, function (license, version, author, email, url, name) {
-    if (!opts.meta.license) opts.meta.license = license
-    if (!opts.meta.version) opts.meta.version = version
-    if (!opts.meta.author) opts.meta.author = author
-    if (!opts.meta.email) opts.meta.email = email
-    if (!opts.meta.url) opts.meta.url = url
-    if (!opts.meta.name) opts.meta.name = name
-
-    return cb(opts)
-  })
-}
-
-function getMeta (conf, cb) {
-  nconf.load({}, function (err, config) {
+function getMeta (opts, cb) {
+  return nconf.load({}, function (err, config) {
     assert.ifError(err)
-    var license = config.get('init.license') || 'ISC'
-    var version = config.get('init.version') || '0.1.0'
-    var author = config.get('init.author.name') || 'Your Name'
-    var email = config.get('init.author.email') || 'your@email.com'
-    var name = config.get('init.author.github') || 'githubName'
-    var url = config.get('init.author.url') || 'https://github.com/' + name + '/' + conf.meta.packageName
-    cb(license, version, author, email, url, name)
+    opts.meta.license = opts.meta.license || config.get('init.license') || 'ISC'
+    opts.meta.version = opts.meta.version || config.get('init.version') || '0.1.0'
+    opts.meta.author  = opts.meta.author || config.get('init.author.name') || 'Your Name'
+    opts.meta.email   = opts.meta.email || config.get('init.author.email') || 'your@email.com'
+    opts.meta.name    = opts.meta.name || config.get('init.author.github') || 'githubName'
+    opts.meta.url     = opts.meta.url || config.get('init.author.url')
+      || 'https://github.com/' + opts.meta.name + '/' + opts.meta.packageName
+    cb(opts)
   })
 }
 
-module.exports = makeConfig
+module.exports = getMeta
