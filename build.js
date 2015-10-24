243290beeb6635fdf8e0ba6b46409019de8f8e35
akileez <git@keeda.net>
2015-10-24 09:28:33 -0400

    update build
    updated underlying tool set. updated descriptions. still WIP

--- build.js
+++ build.js
@@ -43,22 +43,14 @@ var data = {
     '',
     { option: '-h, --help', description: 'display usage information' },
     { option: '-v, --version', description: 'display program version number' },
-    { option: '-d, --dry', description: 'dry run displaying metadata used for generation' },
-    // '',
-    { option: '-r, --repo', description: 'initialize a git repository when generating a private module. [default is none]' },
-    // '',
-    { option: '-g, --github', description: 'new public module. a git repository will be automactically initialized and pushed to gihub.' },
-    // '',
-    { option: '--remote', description: 'process will use the generic git command: `git remote add origin https://github.com/username/repo.git` '},
-    // '',
-    { option: '--no-remote', description: 'do not create a remote repository on github.'},
-    // '',
-    { option: '--dep <string>', description: 'Dependencies. Give a list of node modules to install, i.e., `--dep "lodash moment"` or `--dep "lodash, moment"`. Command executed is [npm install --save packages]'},
-    // '',
-    { option: '--dev <string>', description: 'devDependencies. Give a list of node dev modules to install, i.e., `--dev "tape, istanbul"` or `--dev "tape, istanbul"`. Command executed is [npm install --save-dev devpackages]'},
-    // '',
-    { option: '--verbose', description: 'sliem sddlkj elkjt dher dfjsue sjkhe skjheir glfos sdlfj.' },
-    '',
+    { option: '-d, --dry', description: 'dry run displaying metadata used for generation\n\n' },
+    { option: '-r, --repo', description: 'initialize a git repository when generating a private module. [default is none]\n\n' },
+    { option: '-g, --github', description: 'new public module. a git repository will be automactically initialized and pushed to gihub.\n\n' },
+    { option: '--remote', description: 'process will use the generic git command: \n\n`git remote add origin https://github.com/username/repo.git`\n\n username and repo will be automactically added. Leaving this option off will use `hub` (https://github.com/github/hub) to createthe remote repository. You will have to enter your github username and password.\n\n'},
+    { option: '--no-remote', description: 'do not create a remote repository on github.\n\n'},
+    { option: '--dep <string>', description: 'Dependencies. Give a list of node modules to install, i.e., `--dep "lodash moment"` or `--dep "lodash, moment"`. Command executed is [npm install --save packages]\n\n'},
+    { option: '--dev <string>', description: 'devDependencies. Give a list of node dev modules to install, i.e., `--dev "tape, istanbul"` or `--dev "tape, istanbul"`. Command executed is [npm install --save-dev devpackages]\n\n'},
+    { option: '--verbose', description: 'sliem sddlkj elkjt dher dfjsue sjkhe skjheir glfos sdlfj.\n\n' },
     clrz.underline('Overrides:'),
     '',
     { option: '--desc  <string>', description: 'description for package.json and github repository if using `hub`.'},
@@ -71,7 +63,7 @@ var data = {
 }
 
 var obj = {
-  body : columns(data, {viewWidth: 90, padding: {left: '  ', right: '  '}})
+  body : columns(data, {viewWidth: 85, padding: {left: '  ', right: '  '}})
 }
 
 writeFile(dest, expand(usage.toString(), obj), function (err) {
